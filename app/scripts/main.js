77a968766ea65c7b53bbac73755369c4beb10194
Joshua Smith <joshuawordsmith@gmail.com>
2014-05-27 17:18:57 -0400

    added tests

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+var imgList = ['https://s3.amazonaws.com/ooomf-com-files/ikZyw45kT4m16vHkHe7u_9647713235_29ce0305d2_o.jpg', 'https://s3.amazonaws.com/ooomf-com-files/BIR62RGGjGxN5nrbnzwu_3.jpg', 'https://s3.amazonaws.com/ooomf-com-files/YOfYx7zhTvYBGYs6g83s_IMG_8643.jpg' ];
+
+
+function render (i) {	
+	var url = imgList[i];
+	$('.slider').html('<img src=' +url+ ' class=img-top>');
+};
+
+//sets image url and delays, then renders
+$('.start').click(function (){
+	//sets i to zero, which is used to index the imgList
+	var i = 0;
+
+		//will cycle through the images every 2 seconds forever
+		setInterval(function () {
+		//checks to see if the index of imgList being targeted is undefined, and if so
+		//it will reset the index to zero
+		if (i > (imgList.length - 1)) {
+			i = 0;
+		}
+		//calls the render function and passes the current i counter 
+		//adds one to i after it has been rendered
+		render(i);
+		i ++;
+
+		}, 2000);
+});
+	
--- test/spec/test.js
+++ test/spec/test.js
@@ -2,13 +2,54 @@
 
 (function () {
     'use strict';
+    describe('image array', function(){
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-              var answer = 2;
-              expect(2).to.equal(answer);
-            });
-        });
+    	it('should contain url strings', function (){
+    		
+    		var result = imgList[0];
+    		expect(result).to.have.string('http');
+
+    		var result = imgList[1];
+    		expect(result).to.have.string('http');
+
+    		var result = imgList[2];
+    		expect(result).to.have.string('http');
+    	});
     });
+
+    describe('render function', function () {
+
+    	it('should push the html for the image to the .slider div', function(){
+
+    		render(0);
+
+    		setTimeout(function() {
+    			expect($('.slider').html.length).to.equal(('<img src=https://s3.amazonaws.com/ooomf-com-files/ikZyw45kT4m16vHkHe7u_9647713235_29ce0305d2_o.jpg class=img-top>').length);
+    		},2000)
+    	});
+    });
+
+    describe('start click function', function () {
+    	it('change the image displayed every two seconds', function(){
+    		$('.start').click()
+
+    		setTimeout(function() {
+    			expect($('.slider').html.length).to.equal(('<img src=https://s3.amazonaws.com/ooomf-com-files/ikZyw45kT4m16vHkHe7u_9647713235_29ce0305d2_o.jpg class=img-top>').length);
+    		},4000);
+
+    		setTimeout(function() {
+    			expect($('.slider').html.length).to.equal(('<img src=https://s3.amazonaws.com/ooomf-com-files/BIR62RGGjGxN5nrbnzwu_3.jpg class=img-top>').length);
+    		},6000);
+
+    		setTimeout(function() {
+    			expect($('.slider').html.length).to.equal(('<img src=https://s3.amazonaws.com/ooomf-com-files/YOfYx7zhTvYBGYs6g83s_IMG_8643.jpg class=img-top>').length);
+    		},8000);
+
+    	});
+
+    });
+
+
+
 })();
+
