525fc2d53a595466d1693694709b4a9ac24f7cea
Joshua Smith <joshuawordsmith@gmail.com>
2014-05-29 12:12:12 -0400

    made a constructor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,24 @@
 
-var showInfo = _.template($('.info-master').text());
+// var showInfo = _.template($('.info-master').text());
 
-function renderArr (array, destination){
-	array.forEach(function(module){
-		var rendered = showRepos(module);
-		destination.prepend(rendered);
-	});
-};
-var apiObj;
+// function renderArr (array, destination){
+// 	array.forEach(function(module){
+// 		var rendered = showRepos(module);
+// 		destination.prepend(rendered);
+// 	});
+// };
+// var apiObj;
 
-function renderObj (obj, destination){
-		apiObj = obj;
-		console.log(obj);
-		var rendered = showInfo(obj);
-		destination.prepend(rendered);
-	}
+// function renderObj (obj, destination){
+// 		apiObj = obj;
+// 		console.log(obj);
+// 		var rendered = showInfo(obj);
+// 		destination.prepend(rendered);
+// 	}
 
-$.getJSON('http://gateway.marvel.com:80/v1/public/characters?name=Gambit&apikey=ecbc9156149622b1e7ffc3da73eea458').done(function(obj){
-	renderObj(obj, $('.info-list'));
-});
+// $.getJSON('http://gateway.marvel.com:80/v1/public/comics?format=comic&title=X-men&apikey=ecbc9156149622b1e7ffc3da73eea458').done(function(obj){
+// 	renderObj(obj, $('.info-list'));
+// });
   // .done(function(data) {
   //   console.log( "second success" );
   //   renderArr(data, $('.info-list'));
@@ -41,12 +41,12 @@ $.getJSON('http://gateway.marvel.com:80/v1/public/characters?name=Gambit&apikey=
 // }
 // })
 
-for (var i = 0; i <= (apiObj.data.results[i].length - 1); i++) {
-	
-	if (apiObj.data.results[i].images[0] && apiObj.data.results[i].images !=[]){
-	console.log(i)
-	$('.info-list').prepend('<img src=' + apiObj.data.results[i].images[0].path + '.jpg >');	
-}};
+// for (var i = 0; i <= (apiObj.data.results[i].length - 1); i++) {
+
+// 	if (apiObj.data.results[i].images[0] && apiObj.data.results[i].images !=[]){
+// 	console.log(i)
+// 	$('.info-list').prepend('<img src=' + apiObj.data.results[i].images[0].path + '.jpg >');	
+// }};
 
 
 
@@ -54,28 +54,29 @@ for (var i = 0; i <= (apiObj.data.results[i].length - 1); i++) {
 var imgList = ['https://s3.amazonaws.com/ooomf-com-files/ikZyw45kT4m16vHkHe7u_9647713235_29ce0305d2_o.jpg', 'https://s3.amazonaws.com/ooomf-com-files/BIR62RGGjGxN5nrbnzwu_3.jpg', 'https://s3.amazonaws.com/ooomf-com-files/YOfYx7zhTvYBGYs6g83s_IMG_8643.jpg' ];
 
 
-function render (i) {	
-	var url = imgList[i];
-	$('.slider').html('<img src=' +url+ ' class=img-top>');
-};
+function Render () {	
+var i = 0;
+	//sets i to zero, which is used to index the imgList
+		//will cycle through the images every 2 seconds forever
+	this.loop =	setInterval(function () {
+		//checks to see if the index of imgList being targeted is undefined, and if so
+		//it will reset the index to zero
+		i = i == imgList.length - 1 ? 0      : i += 1;
+		//calls the render function and passes the current i counter 
+		var url = imgList[i];
+		$('.slider').html('<img src=' +url+ ' class=img-top>');
+
+		}, 2000);
+
+	this.stop = clearInterval(this.loop);
+}
 
 //sets image url and delays, then renders
 $('.start').click(function (){
-	//sets i to zero, which is used to index the imgList
-	var i = 0;
-
-		//will cycle through the images every 2 seconds forever
-		setInterval(function () {
-		//checks to see if the index of imgList being targeted is undefined, and if so
-		//it will reset the index to zero
-		if (i > (imgList.length - 1)) {
-			i = 0;
-		}
-		//calls the render function and passes the current i counter 
-		//adds one to i after it has been rendered
-		render(i);
-		i ++;
-
-		}, 2000);
+	render = new Render();
+	render.loop;
 });
+
+//i = i == imgList.length - 1 ? 0      : i += 1;
+//set to   |if this equals       this |do this | else do this
 	
